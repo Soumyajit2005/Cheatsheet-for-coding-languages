cc31b720e2e6217f959c4bcecf7afe3b44bfe87a
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-11-06 22:03:40 +0530

    Improved codes and added new Images

--- C/help.c
+++ C/help.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>                         //header files
+int main()                               //main function
 {
-return(0);
+return(0);                              //returning value to int main()
 }
