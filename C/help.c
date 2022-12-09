45db0575c32a175a61d62f9ecdd7cf3f1fc8de29
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-12-09 23:10:58 +0530

    modified some files and deleted the style.css files for each folders

--- C/help.c
+++ C/help.c
@@ -1,5 +1,10 @@
-#include<stdio.h>                         //header files
-int main()                               //main function
+#include <stdio.h> //header files
+int main()         // main function
 {
-return(0);                              //returning value to int main()
+    // scanf("format_specifier", &variables)
+    // Example
+    int a;
+    scanf("%d", &a);
+    printf("%d", a);
+    return 0; // returning value to int main()
 }
